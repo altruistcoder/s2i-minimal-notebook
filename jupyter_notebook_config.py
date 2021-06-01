77ef2e7b35c4321fd04b5fd67f8b5f5e644e6fd7
Rishabh Aggarwal <31319270+altruistcoder@users.noreply.github.com>
2021-06-01 17:49:27 +0530

    Updated jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -22,3 +22,89 @@ image_config_file = '/opt/app-root/src/.jupyter/jupyter_notebook_config.py'
 if os.path.exists(image_config_file):
     with open(image_config_file) as fp:
         exec(compile(fp.read(), image_config_file, 'exec'), globals())
+
+        
+#######################
+# Directories mapping #
+#######################
+import boto3
+from s3contents import S3ContentsManager
+from hybridcontents import HybridContentsManager
+from notebook.services.contents.largefilemanager import LargeFileManager
+
+
+# We use HybridContentsManager (https://github.com/viaduct-ai/hybridcontents),
+# LargeFileManager for accessing local volumes
+# and S3ContentsManager (https://github.com/danielfrg/s3contents) to connect to the datalake
+c.NotebookApp.contents_manager_class = HybridContentsManager
+
+# Intialize Hybrid Contents Manager with local filesystem
+c.HybridContentsManager.manager_classes = {
+    # Associate the root directory with a LargeFileManager.
+    # This manager will receive all requests that don't fall under any of the
+    # other managers.
+    '': LargeFileManager
+}
+
+# Get S3 credentials from environment variables
+aws_access_key_id = os.environ.get("accessKeyID")
+aws_secret_access_key = os.environ.get("secretAccessKey")
+shared_aws_access_key_id = os.environ.get("sharedAccessKeyID")
+shared_aws_secret_access_key = os.environ.get("sharedSecretAccessKey")
+endpoint_url = os.environ.get("S3_ENDPOINT_URL")
+
+# Add datalake connection information
+if (aws_access_key_id and aws_access_key_id!=None): # Make sure we have usable S3 informations are there before configuring
+    # Initialize S3 connection (us-east-1 seems to be needed even when it is not used, in Ceph for example)
+    s3 = boto3.resource('s3','us-east-2',
+                        endpoint_url=endpoint_url,
+                        aws_access_key_id = aws_access_key_id,
+                        aws_secret_access_key = aws_secret_access_key,
+                        use_ssl = True if 'https' in endpoint_url else False ) 
+    # Enumerate all accessible buckets and create a folder entry in HybridContentsManager
+    for bucket in s3.buckets.all():
+        c.HybridContentsManager.manager_classes.update({bucket.name: S3ContentsManager})
+
+# Add datalake connection information for shared S3
+if (shared_aws_access_key_id and shared_aws_secret_access_key!=None): # Make sure we have usable S3 informations are there before configuring
+    # Initialize S3 connection (us-east-1 seems to be needed even when it is not used, in Ceph for example)
+    shared_s3 = boto3.resource('s3','us-east-2',
+                        endpoint_url=endpoint_url,
+                        aws_access_key_id = shared_aws_access_key_id,
+                        aws_secret_access_key = shared_aws_secret_access_key,
+                        use_ssl = True if 'https' in endpoint_url else False ) 
+    # Enumerate all accessible buckets and create a folder entry in HybridContentsManager
+    for bucket in shared_s3.buckets.all():
+        c.HybridContentsManager.manager_classes.update({bucket.name: S3ContentsManager})
+
+# Initalize arguments for local filesystem
+c.HybridContentsManager.manager_kwargs = {
+    # Args for the LargeFileManager mapped to /directory
+    '': {
+        'root_dir': '/opt/app-root/src'
+    }
+}
+
+# Add datalake connections arguments
+if (aws_access_key_id and aws_access_key_id!=None):
+    # We don't have to reinitialize the connection, thanks for previous "for" not being scoped
+    # Enumerate all buckets and configure access
+    for bucket in s3.buckets.all():
+        c.HybridContentsManager.manager_kwargs.update({bucket.name: {
+            'access_key_id': aws_access_key_id,
+            'secret_access_key': aws_secret_access_key,
+            'endpoint_url': endpoint_url,
+            'bucket': bucket.name
+        } })
+
+# Add datalake connections arguments for Shared S3
+if (shared_aws_access_key_id and shared_aws_secret_access_key!=None):
+    # We don't have to reinitialize the connection, thanks for previous "for" not being scoped
+    # Enumerate all buckets and configure access
+    for bucket in shared_s3.buckets.all():
+        c.HybridContentsManager.manager_kwargs.update({bucket.name: {
+            'access_key_id': shared_aws_access_key_id,
+            'secret_access_key': shared_aws_secret_access_key,
+            'endpoint_url': endpoint_url,
+            'bucket': bucket.name
+        } })
